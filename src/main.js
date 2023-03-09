edd82795d5f3c71a869917509bda3803e0f11a20
Dmytro Stas <dima7stas777@gmail.com>
2023-03-08 22:09:18 -0500

    main.js fixed

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
+import '/js/modals.js';
+import '/js/datepicker.js';
+import '/js/multiprice.js';
+import '/js/phone-mask.js';
+
 // Swiper init
-import './js/swiperInit';
+import '/js/swiperInit';
 
 // Header
-import './js/header.js';
-
-// import './js/modals.js';
-// import './js/datepicker.js';
-// import './js/multiprice.js';
-// import './js/phone-mask.js';
+import '/js/header.js';
