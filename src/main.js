b5dd1db46aee3b819c8b821deec4c463785f5c4f
Dmytro Stas <dima7stas777@gmail.com>
2023-03-08 21:36:13 -0500

    modal fix

--- src/main.js
+++ src/main.js
@@ -3,3 +3,8 @@ import './js/swiperInit';
 
 // Header
 import './js/header.js';
+
+// import './js/modals.js';
+// import './js/datepicker.js';
+// import './js/multiprice.js';
+// import './js/phone-mask.js';
